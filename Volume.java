77faf8f5cfb72d2a2690b03b0c77284771bb8576
deepakCode2 <1209deepak.shukla@gmail.com>
2025-06-11 10:49:45 +0530

    Add files via upload

---
+++ Volume.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Volume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        double height = sc.nextDouble();
+        double volume = Math.PI * radius * radius * height;
+        System.out.println(volume);
+    }
+}
